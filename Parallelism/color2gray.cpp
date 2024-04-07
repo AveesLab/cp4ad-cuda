0c5873ca7ffb1a72bfad35218da08e0b483236ed
DHKing7 <kd4hyun95@kookmin.ac.kr>
2024-04-07 23:27:29 +0900

    Add Makefile

--- Parallelism/color2gray.cpp
+++ Parallelism/color2gray.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <time.h>
 
-#include "opencv2/highgui.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 
 
 int main(void) {
 
-    cv::Mat Color_Img = imread("");
+    cv::Mat Color_Img = cv::imread("bonggu.JPG");
     cv::Mat Gray_Img;
 
     clock_t start = clock();
-    cv2::cvtColor(Color_Img, Gray_Img, COLOR_BGR2GRAY);
+    cv::cvtColor(Color_Img, Gray_Img, cv::COLOR_BGR2GRAY);
     clock_t end = clock();
 
-    double excution_time = (double) (end - start) / CLOCKS_PER_SEC;
+    double execution_time = (double) (end - start) / CLOCKS_PER_SEC;
     printf("Execution time: %d usec\n", (int) (execution_time * 1000000));
     cv::imshow("Color Image", Color_Img);
     cv::imshow("Gray Image", Gray_Img);
